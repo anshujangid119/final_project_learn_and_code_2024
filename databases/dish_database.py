ef5de656d52ed2af98998a11420e2b321f3f3da8
anshujangid119@gmail.com <anshujangid119@gmail.com>
2024-06-18 21:04:20 +0530

    added credential manager

--- databases/dish_database.py
+++ databases/dish_database.py
@@ -1,15 +1,6 @@
 import mysql.connector
 
 class DishDatabase:
-    def __init__(self):
-        self.connection = mysql.connector.connect(
-            host="localhost",
-            user="admin",
-            password = "admin@1234",
-            database="cafeteria"
-        )
-        self.cursor = self.connection.cursor()
-
     def add_dish(self, name, ingredients):
         query = "INSERT INTO dishes (name, ingredients) VALUES (%s, %s)"
         self.cursor.execute(query, (name, ingredients))
